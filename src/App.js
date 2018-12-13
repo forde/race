7fcf319c5726099d50c3049950dbe362122fec26
Konrad <konrad@forde.pl>
2018-12-13 10:13:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import fb from './firebase';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor() {
+        super();
+
+        this.state = {
+            testValue: null,
+            v: '',
+        }
+    }
+
+    componentDidMount() {
+
+        // get data on boot
+        fb.dbRef.once('value').then(snapshot => {
+            this.setState({
+                testValue: snapshot.child('test').val()
+            });
+        });
+
+        // listen for changes
+        fb.dbRef.child('test').on('value', snapshot => {
+            this.setState({
+                testValue: snapshot.val() || null,
+            });
+        });
+    }
+
+    render() {
+        const { testValue, v } = this.state;
+
+        return (
+            <div className="App">
+                <h2>The value in DB is: {testValue}</h2>
+                <input value={v} onChange={e => this.setState({v: e.target.value})} />
+                <button onClick={() => fb.setTestValue(v)} >Set in DB</button>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCAilol4FoHOWzSlph_CRGJJCTOyRowVTM",
+    authDomain: "race-9e4a2.firebaseapp.com",
+    databaseURL: "https://race-9e4a2.firebaseio.com",
+    projectId: "race-9e4a2",
+    storageBucket: "",
+    messagingSenderId: "256809436668"
+};
+
+firebase.initializeApp(config);
+
+const fb = {
+
+    dbRef: firebase.database().ref(),
+
+    setTestValue: value => fb.dbRef.child('test').set(value),
+}
+
+export default fb;
